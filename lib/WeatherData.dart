b051d09362668a0f2bf1fddbc73098523d9d66ad
xushaoyin <xushaoyin@e-future.com.cn>
2019-01-25 17:39:08 +0800

    添加网络依赖包，调用接口，动态获取天气信息，并刷新页面

---
+++ lib/WeatherData.dart
@@ -0,0 +1,30 @@
+class WeatherData {
+  String city;
+  String cond; //天气
+  String tmp; //温度
+  String hum; //湿度
+  String updateTime; //更新时间
+
+  WeatherData({this.city, this.cond, this.tmp, this.hum, this.updateTime});
+
+  factory WeatherData.fromJson(Map<String, dynamic> json) {
+    return WeatherData(
+        city: json['HeWeather6'][0]['basic']['admin_area'] + json['HeWeather6'][0]['basic']['location'],
+        cond: json['HeWeather6'][0]['now']['cond_txt'],
+        tmp: json['HeWeather6'][0]['now']['tmp'] + "°",
+        hum: "湿度  " + json['HeWeather6'][0]['now']['hum'] + "%" + " " +
+            json['HeWeather6'][0]['now']['wind_dir'],
+        updateTime: json['HeWeather6'][0]['update']['loc']
+    );
+  }
+
+  factory WeatherData.empty() {
+    return WeatherData(
+      city: "",
+      cond: "",
+      tmp: "",
+      hum: "",
+      updateTime: "",
+    );
+  }
+}
--- lib/WeatherWidget.dart
+++ lib/WeatherWidget.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_demo/WeatherData.dart';
+import 'package:http/http.dart' as http;
 
 class WeatherWidget extends StatefulWidget {
   @override
@@ -9,6 +13,36 @@ class WeatherWidget extends StatefulWidget {
 }
 
 class WeatherState extends State<WeatherWidget> {
+  WeatherData weatherData = WeatherData.empty();
+
+  WeatherState() {
+    _getWeather('广州');
+  }
+
+  //异步获取天气信息
+  void _getWeather(city) async {
+    print('点我了');
+    WeatherData data = await _fetchWeatherData(city);
+    //获取到信息后，设置状态刷新页面
+    setState(() {
+      weatherData = data;
+    });
+  }
+
+  Future<WeatherData> _fetchWeatherData(city) async {
+    //请求接口获取天气信息
+    final response = await http.get(
+        'https://free-api.heweather.com/s6/weather/now?location=' +
+            city +
+            '&key=a90756aa8a32400b9cbc2d7ec59e9f35');
+    if (response.statusCode == 200) {
+      print(response.body);
+      return WeatherData.fromJson(json.decode(response.body));
+    } else {
+      return WeatherData.empty();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +50,7 @@ class WeatherState extends State<WeatherWidget> {
         fit: StackFit.expand,
         children: <Widget>[
           new Image.asset(
-            "images/weather_bg.jpg",
+            "images/1.jpg",
             fit: BoxFit.fitHeight,
           ),
           new Column(
@@ -25,30 +59,38 @@ class WeatherState extends State<WeatherWidget> {
             children: <Widget>[
               new Container(
                 width: double.infinity,
-                margin: EdgeInsets.only(top: 40.0),
+                margin: EdgeInsets.only(top: 50.0),
                 child: new Text(
-                  "广州市",
+                  weatherData.city,
                   textAlign: TextAlign.center,
                   style: new TextStyle(color: Colors.white, fontSize: 30.0),
                 ),
               ),
               new Container(
                 width: double.infinity,
-                margin: EdgeInsets.only(top: 100.0),
+                margin: EdgeInsets.only(top: 280.0),
                 child: new Column(
                   children: <Widget>[
                     new Text(
-                      "20 °",
+                      weatherData.tmp,
                       style: new TextStyle(color: Colors.white, fontSize: 80.0),
                     ),
                     new Text(
-                      "晴",
+                      weatherData.cond,
                       style: new TextStyle(color: Colors.white, fontSize: 45.0),
                     ),
                     new Text(
-                      "湿度  80%",
+                      weatherData.hum,
                       style: new TextStyle(color: Colors.white, fontSize: 30.0),
                     ),
+                    new FlatButton(
+                        child: Text(weatherData.updateTime,
+                            style: new TextStyle(
+                                color: Colors.white, fontSize: 25.0)),
+                        onPressed: () {
+//                          showAlertDialog(context);
+                          _getWeather('深圳');
+                        }),
                   ],
                 ),
               ),
@@ -57,5 +99,27 @@ class WeatherState extends State<WeatherWidget> {
         ],
       ),
     );
+  }
+
+  void showAlertDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (_) => new AlertDialog(
+                title: Text("请输入要城市名称"),
+                content: Text("广州"),
+                actions: <Widget>[
+                  new FlatButton(
+                    child: new Text("取消"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  new FlatButton(
+                    child: new TextField(),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ]));
   }
 }
